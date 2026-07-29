661ed53ac87748817f5fa5b2395c443dd841320a
Nalandwar Yash Tukaram <yashnalandwar@gmail.com>
2026-07-29 10:37:16 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (41.28%) - LeetSync

---
+++ 68-text-justification/text-justification.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<string> fullJustify(vector<string>& words, int maxWidth) {
+        vector<string> ans;
+        int n = words.size();
+        int i = 0;
+
+        while (i < n) {
+            int j = i;
+            int len = 0;
+
+            while (j < n && len + words[j].size() + (j - i) <= maxWidth) {
+                len += words[j].size();
+                j++;
+            }
+
+            int spaces = maxWidth - len;
+            int gaps = j - i - 1;
+
+            string line;
+
+            if (j == n || gaps == 0) {
+                for (int k = i; k < j; k++) {
+                    if (k > i) line += ' ';
+                    line += words[k];
+                }
+                line += string(maxWidth - line.size(), ' ');
+            } else {
+                int spaceEach = spaces / gaps;
+                int extra = spaces % gaps;
+
+                for (int k = i; k < j; k++) {
+                    line += words[k];
+
+                    if (k < j - 1) {
+                        line += string(spaceEach + (k - i < extra ? 1 : 0), ' ');
+                    }
+                }
+            }
+
+            ans.push_back(line);
+            i = j;
+        }
+
+        return ans;
+    }
+};
